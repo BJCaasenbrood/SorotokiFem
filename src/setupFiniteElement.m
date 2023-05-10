7513fb694cc8efb7ca118c7eeec6508e94989612
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-05-10 17:16:54 +0200

    feat: Add setupFiniteElement and showDilationFem functions
    Added a new function called setupFiniteElement that sets up the finite element model. Also added a new function called showDilationFem that shows the dilation of the finite element model. Modified showContactFem to include thicker contour lines.

---
+++ src/setupFiniteElement.m
@@ -0,0 +1,32 @@
+function Fem = setupFiniteElement(Fem)
+
+%Fem.ElemNDof      = Fem.Dim*cellfun(@length,Fem.Mesh,Element);
+P = meshregularizationfilter(Fem.Mesh, Fem.topology.SpatialFilterRadius,    ...
+    'Periodic',Fem.topology.Periodic);
+
+Fem.topology.SpatialFilter = P;
+
+if (~Fem.options.isAssembled && ~Fem.options.isNonlinear)                   ...
+        || Fem.options.isNonlinear
+    
+    Fem.triplets.i = zeros(sum(Fem.Mesh.ElemNDof.^2),1);
+    
+    Fem.triplets.j  = Fem.triplets.i;
+    Fem.triplets.e  = Fem.triplets.i;
+    Fem.triplets.fi = Fem.triplets.i;
+    Fem.triplets.k  = Fem.triplets.i;
+    Fem.triplets.m  = Fem.triplets.i;
+    Fem.triplets.c  = Fem.triplets.i;
+    Fem.triplets.t  = Fem.triplets.i;
+    Fem.triplets.fb = Fem.triplets.i;
+    Fem.triplets.ft = Fem.triplets.i;
+    Fem.triplets.v  = Fem.triplets.i;
+    
+    Fem.triplets.s  = zeros(Fem.Mesh.NNode,6);
+    Fem.triplets.p  = zeros(Fem.Mesh.NNode,3);
+    Fem.triplets.l  = zeros(Fem.Mesh.NNode,1);
+    Fem.triplets.vj = zeros(Fem.Mesh.NNode,1);
+    
+end
+
+end
