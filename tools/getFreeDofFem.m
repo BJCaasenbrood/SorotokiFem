d9a4102b5a78fa28a6c26d03126b250520fb4938
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-27 14:27:06 +0200

    Fix bug in getFreeDofFem.m
    The code change fixes a bug in the getFreeDofFem.m file. The issue was with the calculation of FixedDofs for 3-dimensional cases. The incorrect variable "Fem.Support" has been replaced with the correct variable "Fem.system.Support". This ensures that the calculation is accurate and produces the expected results.

--- tools/getFreeDofFem.m
+++ tools/getFreeDofFem.m
@@ -7,7 +7,7 @@ if Fem.Dim == 2 && NSupp > 0
         Fem.system.Support(1:NSupp,3).*(2*Fem.system.Support(1:NSupp,1))];
 
 elseif Fem.Dim == 3 && NSupp > 0
-    FixedDofs = [Fem.Support(1:NSupp,2).*(3*Fem.system.Support(1:NSupp,1)-2);
+    FixedDofs = [Fem.system.Support(1:NSupp,2).*(3*Fem.system.Support(1:NSupp,1)-2);
         Fem.system.Support(1:NSupp,3).*(3*Fem.system.Support(1:NSupp,1)-1);
         Fem.system.Support(1:NSupp,4).*(3*Fem.system.Support(1:NSupp,1))];
 end
